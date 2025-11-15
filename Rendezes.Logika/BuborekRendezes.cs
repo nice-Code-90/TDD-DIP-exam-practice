c8887f45541e65846c4424eef617975e627c44d1
Milan Mazik <mazik1990@hotmail.com>
2025-11-15 20:19:22 +0100

    bubble sort: ordering an array - test green

--- Rendezes.Logika/BuborekRendezes.cs
+++ Rendezes.Logika/BuborekRendezes.cs
@@ -6,6 +6,17 @@ public class BuborekRendezes : IRendezesiStrategia
 {
     public void Rendezes(int[] t, ICserelheto cserelo)
     {
-        throw new NotImplementedException();
+        if (t is null)
+        {
+            return;
+        }
+
+        for (int i = 0; i < t.Length - 1; i++)
+        {
+            for (int j = 0; j < t.Length - 1 - i; j++)
+            {
+                if (t[j] > t[j + 1]) cserelo.Csere(ref t[j], ref t[j + 1]);
+            }
+        }
     }
 }
