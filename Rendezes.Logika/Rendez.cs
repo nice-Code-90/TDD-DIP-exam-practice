b0c37c9cb63ac6cd93b8fd9036aa2093218a471b
Milan Mazik <mazik1990@hotmail.com>
2025-11-15 15:27:23 +0100

    chore: Apply .gitignore and remove tracked build artifacts

---
+++ Rendezes.Logika/Rendez.cs
@@ -0,0 +1,22 @@
+namespace Rendezes.Logika;
+
+public class Rendez : ICserelheto
+{
+    private readonly IRendezesiStrategia _strategia;
+
+    // Konstruktor, ami fogadja a stratégiát.
+    public Rendez(IRendezesiStrategia strategia)
+    {
+        _strategia = strategia;
+    }
+
+    // A metódus, amit a tesztünk hív. Egyelőre üres.
+    public void Rendezes(int[] t)
+    {
+    }
+
+    // Az interfész implementálása. Egyelőre üres.
+    public void Csere(ref int a, ref int b)
+    {
+    }
+}
