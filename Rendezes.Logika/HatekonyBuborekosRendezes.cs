5317e020f02a85c62731b7f32694e8394c4674de
Milan Mazik <mazik1990@hotmail.com>
2025-11-15 22:20:20 +0100

    efficient_bubblesort_order: green test

--- Rendezes.Logika/HatekonyBuborekosRendezes.cs
+++ Rendezes.Logika/HatekonyBuborekosRendezes.cs
@@ -6,6 +6,23 @@ public class HatekonyBuborekosRendezes : IRendezesiStrategia
 {
     public void Rendezes(int[] t, ICserelheto cserelo)
     {
-        throw new NotImplementedException();
+        if (t is null)
+        {
+            return;
+        }
+
+        for (int i = 0; i < t.Length - 1; i++)
+        {
+            bool voltCsere = false;
+            for (int j = 0; j < t.Length - 1 - i; j++)
+            {
+                if (t[j] > t[j + 1])
+                {
+                    cserelo.Csere(ref t[j], ref t[j + 1]);
+                    voltCsere = true;
+                }
+            }
+            if (!voltCsere) break;
+        }
     }
 }
